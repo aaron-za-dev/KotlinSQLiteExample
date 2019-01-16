cf0ccd6a228aae8d34c7c76b6ef8108fddb6c7fb
Aaron <aaronzadev@gmail.com>
2019-01-15 23:03:42 -0600

    Added classes for ReciclerView

---
+++ app/src/main/java/com/zadev/aaron/kotlinsqliteexample/models/ItemOperation.kt
@@ -0,0 +1,4 @@
+package com.zadev.aaron.kotlinsqliteexample.models
+
+class ItemOperation {
+}
---
+++ app/src/main/java/com/zadev/aaron/kotlinsqliteexample/models/Operation.kt
@@ -0,0 +1,3 @@
+package com.zadev.aaron.kotlinsqliteexample.models
+
+class Operation (var IDOperation : Int, var DateOperation : String, var AmountOperation : Float,  var IDType : Int)
